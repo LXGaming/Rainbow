a7f5f64f69a886ab80e497b31c5e4f963b66ca07
Alex Thomson <LXGaming@users.noreply.github.com>
2018-06-13 09:51:22 +1200

    Fix Reference

--- src/main/java/nz/co/lolnet/rainbow/util/Reference.java
+++ src/main/java/nz/co/lolnet/rainbow/util/Reference.java
@@ -22,7 +22,7 @@ public class Reference {
     public static final String PLUGIN_NAME = "Rainbow";
     public static final String PLUGIN_VERSION = "1.0.0";
     public static final String DESCRIPTION = "Wear the Rainbow";
-    public static final String AUTHORS = "LX_Gaming";
-    public static final String SOURCE = "https://github.com/lolnetnz/Rainbow";
-    public static final String WEBSITE = "https://www.lolnet.co.nz";
+    public static final String AUTHORS = "James137137, LX_Gaming";
+    public static final String SOURCE = "https://github.com/lolnetnz/Rainbow/";
+    public static final String WEBSITE = "https://www.lolnet.co.nz/";
 }
